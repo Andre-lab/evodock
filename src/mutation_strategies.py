d5052f9e466abf1ceb7511c286deaf6bfa61c026
daniel <daniel.varela@udc.es>
2022-03-29 15:36:40 +0200

    Fix bug at local search processing in trial creator

--- src/mutation_strategies.py
+++ src/mutation_strategies.py
@@ -1,4 +1,5 @@
 import random
+from operator import itemgetter
 
 
 def ensure_bounds(vec, bounds):
@@ -49,6 +50,7 @@ class Strategy:
         self.bounds = [(-1, 1)] * 6
 
     def select_parents(self, j, population, gen_scores):
+        self.mutate = random.uniform(0.05, 0.7)
         candidates = list(range(0, len(population)))
         candidates.remove(j)
         random_index = random.sample(candidates, 3)
@@ -79,6 +81,7 @@ class StrategyRandom(Strategy):
 
 class StrategyBest(Strategy):
     def select_parents(self, j, population, gen_scores):
+        self.mutate = random.uniform(0.05, 0.7)
         candidates = list(range(0, len(population)))
         candidates.remove(j)
         random_index = random.sample(candidates, 3)
@@ -120,3 +123,57 @@ class StrategyCurrent(Strategy):
         x_3 = population[random_index[2]].genotype
 
         return x_1, x_2, x_3
+
+
+class StrategyTriangular(Strategy):
+    def update_best_and_worst(self, gen_scores):
+        self.best = min(list(enumerate(gen_scores)), key=itemgetter(1))[1]
+        self.worst = max(list(enumerate(gen_scores)), key=itemgetter(1))[1]
+        print(self.best, self.worst)
+
+    def select_parents(self, j, population, gen_scores):
+        candidates = list(range(0, len(population)))
+        candidates.remove(j)
+        random_index = random.sample(candidates, 3)
+
+        trial_1 = population[random_index[0]]
+        trial_2 = population[random_index[1]]
+        trial_3 = population[random_index[2]]
+        score_1 = gen_scores[random_index[0]]
+        score_2 = gen_scores[random_index[1]]
+        score_3 = gen_scores[random_index[2]]
+        data = [(trial_1, score_1), (trial_2, score_2), (trial_3, score_3)]
+        data = sorted(data, key=lambda x: x[1])
+
+        x_1 = data[0][0].genotype
+        x_2 = data[1][0].genotype
+        x_3 = data[2][0].genotype
+
+        return x_1, x_2, x_3
+
+    def update_weights(self, population):
+        p1 = 1.0
+        p2 = random.uniform(0.75, 1)
+        p3 = random.uniform(0.5, p2)
+        self.w1 = p1 / sum([p1, p2, p3])
+        self.w2 = p2 / sum([p1, p2, p3])
+        self.w3 = p3 / sum([p1, p2, p3])
+
+    def create_donor(self, j, population, gen_scores):
+        self.update_weights(population)
+        self.mutate = random.uniform(0.05, 0.7)
+        x_1, x_2, x_3 = self.select_parents(j, population, gen_scores)
+        v_donor = []
+        F = self.mutate
+        for g in range(len(x_1)):
+            conv = self.w1 * x_1[g] + self.w2 * x_2[g] + self.w3 * x_3[g]
+            mut = (
+                conv
+                + F * (x_1[g] - x_2[g])
+                + F * (x_2[g] - x_3[g])
+                + F * (x_1[g] - x_3[g])
+            )
+            v_donor.append(mut)
+
+        v_donor = ensure_bounds(v_donor, self.bounds)
+        return v_donor
